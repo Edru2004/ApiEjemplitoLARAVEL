f608664e55784320daa732938e9f3b9b9f23a95d
Dulce Rubi Barragan Peñaloza <[200~l22240021@smartin.tecnm.mx>
2026-03-30 22:29:02 -0600

    Version ultra simple sin errores

--- app/Http/Controllers/API/UsuarioController.php
+++ app/Http/Controllers/API/UsuarioController.php
@@ -8,37 +8,32 @@ use App\Http\Controllers\Controller;
 
 class UsuarioController extends Controller
 {
-    // LISTAR TODOS
     public function index()
     {
         return response()->json(Usuario::all(), 200);
     }
 
-    // CREAR
     public function store(Request $request)
     {
         $usuario = Usuario::create($request->all());
-        return response()->json(['message' => 'Usuario creado', 'data' => $usuario], 201);
+        return response()->json($usuario, 201);
     }
 
-    // MOSTRAR UNO
     public function show($id)
     {
         return response()->json(Usuario::findOrFail($id));
     }
 
-    // ACTUALIZAR
     public function update(Request $request, $id)
     {
         $usuario = Usuario::findOrFail($id);
         $usuario->update($request->all());
-        return response()->json(['message' => 'Usuario actualizado']);
+        return response()->json($usuario);
     }
 
-    // ELIMINAR
     public function destroy($id)
     {
         Usuario::destroy($id);
-        return response()->json(['message' => 'Usuario eliminado']);
+        return response()->json(['message' => 'Eliminado']);
     }
 }
