a749e38f4d1899561f5b18f6f31b5f1c858b3022
Dulce Rubi Barragan Peñaloza <[200~l22240021@smartin.tecnm.mx>
2026-03-31 20:47:40 -0600

    Corrigiendo migración de carreras

--- database/migrations/2026_04_01_011520_create_carreras_table.php
+++ database/migrations/2026_04_01_011520_create_carreras_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('carreras', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre'); // <-- Agregamos el nombre
+            $table->text('descripcion')->nullable(); // <-- Agregamos la descripción opcional
             $table->timestamps();
         });
     }
